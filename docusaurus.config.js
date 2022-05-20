c9a2e0231d00a8e50288b8bc4a20b49cba4ebb39
notAperson535 <95918679+notAperson535@users.noreply.github.com>
2022-05-20 13:53:00 -0400

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -37,7 +37,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/notAperson535/Win11-HTML-Tutorial/tree/website/docs/',
+            'https://github.com/notAperson535/Win11-HTML-Tutorial/tree/website/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
